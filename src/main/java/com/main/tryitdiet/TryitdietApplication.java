254bb7e8cb531a93fefd159de16ae810a31f7f9f
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-19 11:03:25 -0500

    Initial commit: project setup

---
+++ src/main/java/com/main/tryitdiet/TryitdietApplication.java
@@ -0,0 +1,13 @@
+package com.main.tryitdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryitdietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryitdietApplication.class, args);
+    }
+
+}
