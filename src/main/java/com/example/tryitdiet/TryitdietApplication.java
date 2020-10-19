56d0aef59395912708c5dcdf6fd64e47d70f985e
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-19 11:09:31 -0500

    Changed directory name for package

--- src/main/java/com/example/tryitdiet/TryitdietApplication.java
+++ src/main/java/com/example/tryitdiet/TryitdietApplication.java
@@ -1,4 +1,4 @@
-package com.main.tryitdiet;
+package com.example.tryitdiet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/tryitdiet/TryitdietApplicationTests.java
+++ src/test/java/com/example/tryitdiet/TryitdietApplicationTests.java
@@ -1,4 +1,4 @@
-package com.main.tryitdiet;
+package com.example.tryitdiet;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
