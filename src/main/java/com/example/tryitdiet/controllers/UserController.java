fa3b09cd1141b0db0a12642b9555464c6d071a73
Brian Barton <brian.n.barton@gmail.com>
2020-10-21 12:39:34 -0600

    commiting changes to partials

--- src/main/java/com/example/tryitdiet/controllers/UserController.java
+++ src/main/java/com/example/tryitdiet/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.example.tryitdiet.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class UserController {
+
+
+    @GetMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "<h1>This string is supposed to be the index page</h1>";
+
+    }
+
 }
