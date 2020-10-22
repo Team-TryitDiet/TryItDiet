a4baea1d7335e1582960ed2c510a396c8d1fc8bc
zihan zhao <zihanhannah@gmail.com>
2020-10-21 21:17:24 -0500

    model of user

---
+++ src/main/java/com/example/tryitdiet/models/User.java
@@ -0,0 +1,34 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = true, length = 12)
+    private String phone_number;
+
+    @Column(nullable = false)
+    private boolean is_admin;
+
+    @Column(nullable = false)
+    private boolean is_banned;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Post> posts;
+
+}
+
