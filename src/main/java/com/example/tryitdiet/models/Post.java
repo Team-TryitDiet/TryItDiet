22192cf34d1aea3f41b827c1bbe4cfb9aa0bac12
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-22 14:16:00 -0500

    Added the Post model. Working on building out the Post Repository and Post Controller.

--- src/main/java/com/example/tryitdiet/models/Post.java
+++ src/main/java/com/example/tryitdiet/models/Post.java
@@ -1,4 +1,78 @@
 package com.example.tryitdiet.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 250)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Date date;
+
+    // Set relationship between Post and user
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Default Constructor
+    public Post() {}
+
+    // Constructor
+    public Post(long id, String title, Date date, String description) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
