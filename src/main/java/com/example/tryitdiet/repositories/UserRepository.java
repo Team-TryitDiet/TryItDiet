297104b271cc738956ad8b4f2b519d55970a6a11
zihan zhao <zihanhannah@gmail.com>
2020-10-22 00:23:51 -0500

    keep working on user, with usercontroller,register

--- src/main/java/com/example/tryitdiet/repositories/UserRepository.java
+++ src/main/java/com/example/tryitdiet/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.tryitdiet.repositories;
 
-public class UserRepository {
+import com.example.tryitdiet.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+//    User findByUsername(String username);
 }
