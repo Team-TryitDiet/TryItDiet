7953366b6ccf0bc534dcd1dec194b9d6ca60411c
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-26 00:22:03 -0500

    Added in the ingredients model and setup the member variables/properties, constructors, getters and setters. Need to work on the many-to-many relationship between recipes and ingredients for now.

---
+++ src/main/java/com/example/tryitdiet/models/Ingredient.java
@@ -0,0 +1,41 @@
+package com.example.tryitdiet.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    // Many to Many Relationships
+
+    // Constructors
+    public Ingredient() {}
+
+    public Ingredient(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
