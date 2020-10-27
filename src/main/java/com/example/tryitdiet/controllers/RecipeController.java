752882acee2807a91618fad4b88be315eb1d9b04
zihan zhao <zihanhannah@gmail.com>
2020-10-26 22:17:46 -0500

    working on the diets and recipe

--- src/main/java/com/example/tryitdiet/controllers/RecipeController.java
+++ src/main/java/com/example/tryitdiet/controllers/RecipeController.java
@@ -1,8 +1,10 @@
 package com.example.tryitdiet.controllers;
 
+import com.example.tryitdiet.models.Diet;
 import com.example.tryitdiet.models.Post;
 import com.example.tryitdiet.models.Recipe;
 import com.example.tryitdiet.models.User;
+import com.example.tryitdiet.repositories.DietRepository;
 import com.example.tryitdiet.repositories.PostRepository;
 import com.example.tryitdiet.repositories.RecipeRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,27 +17,53 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class RecipeController {
 
+
+
+    private final DietRepository dietRepo;
     // Dependency Injection
     private final RecipeRepository recipeRepo;
     private final PostRepository postRepo;
 
-    // RecipeController Constructor
-    public RecipeController(RecipeRepository recipeRepo, PostRepository postRepo) {
-        this.recipeRepo = recipeRepo;
-        this.postRepo = postRepo;
-    }
+//    // RecipeController Constructor
+//    public RecipeController(RecipeRepository recipeRepo, PostRepository postRepo) {
+//        this.recipeRepo = recipeRepo;
+//        this.postRepo = postRepo;
+//    }
+public RecipeController(DietRepository dietRepo, RecipeRepository recipeRepo, PostRepository postRepo) {
+    this.dietRepo = dietRepo;
+    this.recipeRepo = recipeRepo;
+    this.postRepo = postRepo;
+}
 
-    // Create Recipe Get Method
+    // Create Recipe and diets Get Method
     @GetMapping("/posts/recipe")
     public String createRecipe(Model model) {
         // add a brand new post and a brand new recipe to the model
         model.addAttribute("post", new Post());
         model.addAttribute("recipe", new Recipe());
+        List<Diet> dietsList = dietRepo.findAll();
+        model.addAttribute("dietsList",dietsList);
+        return "recipes/create";
+    }
+    //create Diets for recipe Post Method
+    @PostMapping("/recipe/diets/create")
+    public String createDiets(@RequestParam(name = "diet") List<Diet> diets,
+                              @RequestParam(name = "checked") List<Boolean> checked,@ModelAttribute Recipe recipe){
+        List<Diet> diets1 = new ArrayList<>();
+        for(int i =0; i < diets.size();i++){
+            if(checked.get(i)){
+                diets1.add(diets.get(i));
+            }
+        }
+        recipe.setDiets(diets1);
+        recipeRepo.save(recipe);
         return "recipes/create";
     }
 
@@ -95,4 +123,6 @@ public class RecipeController {
         postRepo.save(post);
         return "redirect:/posts";
     }
+
+
 }
