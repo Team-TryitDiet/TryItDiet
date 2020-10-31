57067a80c327cf81fa4bc34e7798fe227da3c493
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-31 11:18:25 -0500

    Got the ingredients.json to appear onto the chart, but still need to figure out how to select the items and return those selections back to the controller. Also need to place this feature into the recipe creation page.

--- src/main/resources/static/js/ingredients.js
+++ src/main/resources/static/js/ingredients.js
@@ -1,4 +1,12 @@
-(function($) {
-
-    $('#ingredients').dataTable();
-})(jQuery);
+$(document).ready( async function () {
+    const data = await fetch("/ingredients.json").then(result => result.json());
+    console.log(data);
+    $('#ingredients').DataTable({
+        data: data,
+        select: true,
+        columns: [
+            { data: "id" },
+            { data: "name" }
+        ]
+    });
+} );
