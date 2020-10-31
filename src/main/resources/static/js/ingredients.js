ad558b8a437784bd67cedc57ef18d57acac78baa
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-10-31 13:31:50 -0500

    Added in the scroll feature onto the datatable, but still need to figure out the selection and how to get the data back to the controller

--- src/main/resources/static/js/ingredients.js
+++ src/main/resources/static/js/ingredients.js
@@ -1,12 +1,12 @@
 $(document).ready( async function () {
     const data = await fetch("/ingredients.json").then(result => result.json());
-    console.log(data);
-    $('#ingredients').DataTable({
+    const table = $('#ingredients').DataTable({
+        scrollY: 200,
         data: data,
-        select: true,
         columns: [
             { data: "id" },
             { data: "name" }
         ]
     });
+
 } );
