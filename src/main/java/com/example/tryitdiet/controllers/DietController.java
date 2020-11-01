d72435dd142a4446f8368f1a9f7da1153237c11c
zihan zhao <zihanhannah@gmail.com>
2020-10-31 23:08:53 -0500

    make the diets on navbar work

--- src/main/java/com/example/tryitdiet/controllers/DietController.java
+++ src/main/java/com/example/tryitdiet/controllers/DietController.java
@@ -2,66 +2,126 @@ package com.example.tryitdiet.controllers;
 
 import com.example.tryitdiet.models.Diet;
 import com.example.tryitdiet.repositories.DietRepository;
+import com.example.tryitdiet.repositories.IngredientRepository;
 import com.example.tryitdiet.repositories.PostRepository;
 import com.example.tryitdiet.repositories.RecipeRepository;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class DietController {
+    private DietRepository dietRepo;
+
+
+    public DietController(DietRepository dietRepo, PostRepository postRepo, IngredientRepository ingredientRepo) {
+        this.dietRepo = dietRepo;
+
+    }
 
     @GetMapping("/diets/dairy-free")
-    public String showDairyFreePage() {
+    public String showDairyFreePage(Model model) {
+        if ("dairy-free" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("dairy-free");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/dairy-free";
     }
 
     @GetMapping("/diets/gluten-free")
-    public String showGlutenFreePage() {
+    public String showGlutenFreePage(Model model) {
+        if ("gluten-free" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("gluten-free");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/gluten-free";
     }
 
     @GetMapping("/diets/high-protein")
-    public String showHighProteinPage() {
+    public String showHighProteinPage(Model model) {
+        if ("high-protein" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("high-protein");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/high-protein";
     }
 
     @GetMapping("/diets/keto")
-    public String showKetoPage() {
+    public String showKetoPage(Model model) {
+        if ("keto" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("keto");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/keto";
     }
 
     @GetMapping("/diets/paleo")
-    public String showPaleoPage() {
+    public String showPaleoPage(Model model) {
+        if ("paleo" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("paleo");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/paleo";
     }
 
     @GetMapping("/diets/pescatarian")
-    public String showPescatarianPage() {
+    public String showPescatarianPage(Model model) {
+        if ("pescatarian" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("pescatarian");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/pescatarian";
     }
 
     @GetMapping("/diets/mediterranean")
-    public String showMediterraneanPage() {
+    public String showMediterraneanPage(Model model) {
+        if ("mediterranean" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("mediterranean");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/mediterranean";
     }
 
     @GetMapping("/diets/vegan")
-    public String showVeganPage() {
+    public String showVeganPage(Model model) {
+        if ("vegan" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("vegan");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/vegan";
     }
 
     @GetMapping("/diets/vegetarian")
-    public String showVegetarianPage() {
+    public String showVegetarianPage(Model model) {
+        if ("vegetarian" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("vegetarian");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/vegetarian";
     }
 
     @GetMapping("/diets/other")
-    public String showOtherPage() {
+    public String showOtherPage(Model model) {
+        if ("other" != null) {
+            List<Diet> diets = dietRepo.findByTitleContaining("other");
+            System.out.println(diets);
+            model.addAttribute("diets", diets);
+        }
         return "diets/other";
     }
 }
--- src/main/java/com/example/tryitdiet/repositories/DietRepository.java
+++ src/main/java/com/example/tryitdiet/repositories/DietRepository.java
@@ -2,6 +2,9 @@ package com.example.tryitdiet.repositories;
 import com.example.tryitdiet.models.Diet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DietRepository extends JpaRepository<Diet, Long>{
+    List<Diet> findByTitleContaining(String title);
 }
 
