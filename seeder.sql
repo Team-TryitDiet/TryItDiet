d9bfe5737104b3b0b00fcbf0a031dbc307a5cf1a
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-11-09 09:02:45 -0600

    Made a few changes to the seeder file

--- seeder.sql
+++ seeder.sql
@@ -17,13 +17,45 @@ VALUES ('dairy-free'),
 INSERT INTO posts (title, date, user_id, recipe_id)
 VALUES ('chicken alfredo', CURDATE(), 1, 1),
        ('beef stroganoff', CURDATE(), 1, 2),
-       ('chicken & rice', CURDATE(), 1, 3);
+       ('chicken & rice', CURDATE(), 1, 3),
+       ('Lemon Chicken with Orzo', CURDATE(), 2, 4),
+       ('Baked Pot Stickers with Dipping Sauce', CURDATE(), 2, 5);
 
 INSERT INTO recipes (preparation, notes)
 VALUES
 ('salt, pepper, heavy whipping cream, chicken, pasta, cumin', 'some notes on chicken alfredo'),
 ('How to: beef, onion, garlic, butter, pasta, carrots, celery', 'some salt, pepper, beef bouillon'),
-('salt, pepper, rice, butter, chicken bouillon', 'some more notes on chicken and rice');
+('salt, pepper, rice, butter, chicken bouillon', 'some more notes on chicken and rice'),
+('1/3 cup all-purpose flour
+1 teaspoon garlic powder
+1 pound boneless skinless chicken breasts
+3/4 teaspoon salt, divided
+1/2 teaspoon pepper
+2 tablespoons olive oil
+1 can (14-1/2 ounces)
+reduced-sodium chicken broth
+1-1/4 cups uncooked whole wheat orzo pasta
+2 cups chopped fresh spinach
+1 cup grape tomatoes
+halved
+3 tablespoons lemon juice
+2 tablespoons minced fresh basil
+Lemon wedges, optional'),
+('2 cups finely chopped cooked chicken breast
+1 can (8 ounces) water chestnuts, drained and chopped
+4 green onions, thinly sliced
+1/4 cup shredded carrots
+1/4 cup reduced-fat mayonnaise
+1 large egg white
+1 tablespoon reduced-sodium soy sauce
+1 garlic clove, minced
+1 teaspoon grated fresh gingerroot
+48 wonton wrappers
+Cooking spray
+SAUCE:
+1/2 cup jalapeno pepper jelly
+1/4 cup rice vinegar
+2 tablespoons reduced-sodium soy sauce');
 
 INSERT INTO recipes_ingredients (recipe_id, ingredient_id)
 VALUES (1, 3),
@@ -42,9 +74,130 @@ VALUES (1, 3),
        (3, 8),
        (3, 14),
        (3, 15),
-       (3, 16);
+       (3, 16),
+       (4, 23),
+       (5, 8);
+
 INSERT INTO recipes_diets(recipe_id, diet_id)
     VALUES (1, 2),
     (2, 2),
     (2, 3),
     (3, 1);
+
+# INSERT INTO ingredients (name)
+# VALUES
+# ('erythritol'),
+# ('xylitol'),
+# ('splenda'),
+# ('honey'),
+# ('molasses'),
+# ('white sugar'),
+# ('brown sugar'),
+# ('white vinegar'),
+# ('balsamic vinegar'),
+# ('apple cider vinegar'),
+# ('baking soda'),
+# ('baking powder'),
+# ('cocoa powder'),
+# ('olive oil'),
+# ('peanut oil'),
+# ('coconut oil'),
+# ('spray oil'),
+# ('sea salt'),
+# ('milled pepper'),
+# ('basil'),
+# ('oregano'),
+# ('parsley'),
+# ('garlic powder'),
+# ('onion powder'),
+# ('cinnamon'),
+# ('nutmeg'),
+# ('chili'),
+# ('cumin'),
+# ('thyme'),
+# ('fennel'),
+# ('vanilla extract'),
+# ('almond extract'),
+# ('maple extract'),
+# ('mint extract'),
+# ('orange extract'),
+# ('rum extract'),
+# ('almond flour'),
+# ('coconut flour'),
+# ('flour'),
+# ('almond meal'),
+# ('coconut'),
+# ('protein powder'),
+# ('flax seed meal'),
+# ('thickener'),
+# ('chia seeds'),
+# ('soy sauce'),
+# ('worcestershire sauce'),
+# ('kitchen bouquet'),
+# ('onion'),
+# ('green onions'),
+# ('garlic'),
+# ('jalapeno slices'),
+# ('black olives'),
+# ('ro-tel'),
+# ('iced tea bags'),
+# ('mint tea bags'),
+# ('pizza sauce'),
+# ('organic beef broth'),
+# ('organic chicken broth'),
+# ('pumpkin seeds'),
+# ('sunflower seeds'),
+# ('macadamia nuts'),
+# ('almonds'),
+# ('walnuts'),
+# ('pine nuts'),
+# ('cashews'),
+# ('pecans'),
+# ('sun-dried tomatoes'),
+# ('roasted red peppers'),
+# ('artichoke hearts'),
+# ('pesto'),
+# ('alfredo sauce'),
+# ('pumpkin'),
+# ('feta'),
+# ('medium cheddar'),
+# ('sharp cheddar'),
+# ('parmesan'),
+# ('cream cheese'),
+# ('shredded mozzarella'),
+# ('heavy whipping cream'),
+# ('butter'),
+# ('sour cream'),
+# ('beef'),
+# ('eggs'),
+# ('pasta'),
+# ('chicken'),
+# ('bacon'),
+# ('salami'),
+# ('pepperoni'),
+# ('green beans'),
+# ('grape tomatoes'),
+# ('pickles'),
+# ('cucumber'),
+# ('zucchini'),
+# ('cauliflower'),
+# ('spinach'),
+# ('mushrooms'),
+# ('lettuce'),
+# ('lemon juice'),
+# ('green olives'),
+# ('mayonnaise'),
+# ('ketchup'),
+# ('yellow mustard'),
+# ('relish'),
+# ('brussels sprouts'),
+# ('carrots'),
+# ('ginger root'),
+# ('green'),
+# ('red bell peppers'),
+# ('lime juice'),
+# ('blueberries'),
+# ('strawberries'),
+# ('barbecue sauce'),
+# ('mayonnaise'),
+# ('snap peas');
